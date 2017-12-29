e7fd3983e31af6b9545054e01e96fb774a1848f9
Shafeen M <sodl02@gmail.com>
2017-12-29 18:28:27 -0500

    created and added in the <resume> component for the Profile view

---
+++ web/client/ng-client/view-profile/resume.component.js
@@ -0,0 +1,47 @@
+angular.module('basicMEAN')
+    .component('resume', {
+        templateUrl: '/partials/view-profile/resume',
+        bindings: {},
+        controller: 'ResumeController'
+    })
+    .controller('ResumeController', [function () {
+        const ctrl = this;
+
+        ctrl.data = {};
+        ctrl.data.workExperience = [
+            {
+                badgeClass: 'warning',
+                badgeIconClass: 'glyphicon-briefcase',
+                title: 'Senior Engineer',
+                company: 'The Acme Group',
+                dates: 'April 2014 - Present',
+                content: 'This is what I do here.'
+            },
+            {
+                badgeClass: 'info',
+                badgeIconClass: 'glyphicon-briefcase',
+                title: 'Engineer',
+                company: 'The Acme Group',
+                dates: 'April 2013 - March 2014',
+                content: 'This is what I did there.'
+            },
+            {
+                badgeClass: 'info',
+                badgeIconClass: 'glyphicon-briefcase',
+                title: 'Associate Engineer',
+                company: 'The Acme Group',
+                dates: 'April 2012 - March 2013',
+                content: 'This is what I did there.'
+            },
+            {
+                badgeClass: 'info',
+                badgeIconClass: 'glyphicon-briefcase',
+                title: 'Instructional Aide',
+                company: 'The Acme Group',
+                dates: 'April 2011 - March 2012',
+                content: 'This is what I did there.'
+            },
+        ];
+
+
+    }]);
