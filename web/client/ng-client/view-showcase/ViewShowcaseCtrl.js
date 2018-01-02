e8e927230c96225b714eb2b84b2460a4a5b36472
Shafeen M <sodl02@gmail.com>
2018-01-01 19:26:52 -0500

    added a basic showcase view to display projects

--- web/client/ng-client/view-showcase/ViewShowcaseCtrl.js
+++ web/client/ng-client/view-showcase/ViewShowcaseCtrl.js
@@ -5,8 +5,9 @@ angular.module('basicMEAN')
     self.message = 'This is some more text relevant to view 2, but is stored in the View2Ctrl.';
 
     self.$onInit = function () {
-        Settings.init.then(function () {
-            self.message = Settings.data.view2Msg;
+        Settings.init.then(function (settingsData) {
+            self.message = settingsData.view2Msg;
+            self.showcaseData = settingsData.showcaseView;
         })
     };
 
